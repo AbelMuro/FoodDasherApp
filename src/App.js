dce471d25048fb3c4cce92d2a6ddc065c6fcfaee
abelmuro <abelmuro93@gmail.com>
2024-05-04 17:42:49 -0700

    commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, {useEffect} from 'react';
+import { SafeAreaView, Text } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+
+function App() {
+
+    useEffect(() => {
+        async function FS() {
+            await firestore().collection(`my collection`).doc('userInfo').set({}); 
+        }
+
+        FS();
+        
+    }, [])
+
+
+    return (
+        <SafeAreaView>
+            <Text>
+                Hello World
+            </Text>
+        </SafeAreaView>
+    );
+}
+
+export default App;

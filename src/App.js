f39e8a6a4692a45e8b145031c429b3520528b9fb
AbelMuro <abelmuro93@gmail.com>
2024-05-08 22:33:38 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text} from 'react-native';
 import Home from './Pages/Home';
 import Search from './Pages/Search';
 import NavigationBar from './Components/NavigationBar';
 import { NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator} from '@react-navigation/native-stack';
+import firestore from '@react-native-firebase/firestore';
 
 const Stack = createNativeStackNavigator();
 

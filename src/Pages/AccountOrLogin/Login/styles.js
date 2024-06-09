f1e0b01fd14a2c935b0fb5a26cd21c3f2d78d830
abelmuro <abelmuro93@gmail.com>
2024-06-08 17:55:32 -0700

    commit

--- src/Pages/AccountOrLogin/Login/styles.js
+++ src/Pages/AccountOrLogin/Login/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.ImageBackground`
     width: 100%;
-    height: 100%;
+    min-height: 500px;
     display: flex;
     justify-content: center;
     align-items: center;
